ea98622b0f00826fbd512ed9e63070c32d1e8820
Ramesh Mhetre <mhetreramesh@gmail.com>
2018-02-14 12:08:26 +0530

    Add to cart button added
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -8,6 +8,7 @@
   <ul v-else>
     <li v-for="product in products">
       {{ product.title }} - {{ product.price }}
+      <button @click="addProductToCart(product)">Add to cart </button>
     </li>
   </ul>
   </div>
@@ -23,6 +24,11 @@ export default {
     computed: {
         products() {
             return this.$store.getters.availableProducts
+        }
+    },
+    methods: {
+        addProductToCart(product) {
+            this.$store.dispatch('addProductToCart', product)
         }
     },
     created() {
