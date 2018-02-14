e664aad59d9db7208c615c216323caf8d6250f3e
Ramesh Mhetre <mhetreramesh@gmail.com>
2018-02-14 11:47:46 +0530

    Basic app with vuex
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

---
+++ src/components/ProductList.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <h1>Product List</h1>
+    <img
+        v-if="loading"
+        src="https://loading.io/spinners/balls/index.circle-slack-loading-icon.gif"
+    >
+  <ul v-else>
+    <li v-for="product in products">
+      {{ product.title }} - {{ product.price }}
+    </li>
+  </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            loading: false
+        }
+    },
+    computed: {
+        products() {
+            return this.$store.getters.availableProducts
+        }
+    },
+    created() {
+        this.loading = true
+        this.$store.dispatch('fetchProducts')
+        .then(() => this.loading = false)
+    }
+}
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,42 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import shop from '@/api/shop'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: { //data
+        products: []
+    },
+    getters: { // computed properties
+        availableProducts (state, getters) {
+            return state.products.filter(product => product.inventory > 0)
+        }
+    },
+    actions: {
+        fetchProducts({commit}) {
+            return new Promise((resolve, reject) => {
+                shop.getProducts(products => {
+                    commit('setProducts', products)
+                    resolve()
+                })
+            })
+        },
+        addToCart(context, product) {
+            if (product.inventory > 0) {
+                context.commit('pushProductToCart', product)
+            } else {
+                // show out of stock message
+            }
+        }
+    },
+    mutations: {
+        // set & update state
+        setProducts(state, products) {
+            state.products = products
+        },
+        pushProductToCart(state, product) {
+
+        }
+    }
+})
